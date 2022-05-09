1455ec7f95658962a62faeb80a0bef64bc51972c
xusandrew <65879913+xusandrew@users.noreply.github.com>
2022-05-09 13:31:32 -0400

    Update XuSodoku.java

--- XuSodoku.java
+++ XuSodoku.java
@@ -38,7 +38,7 @@ public class XuSodoku extends Applet implements ActionListener {
 					{ 6, 0, 2, 0, 3, 0, 8, 9, 0 },
 					{ 0, 0, 0, 2, 0, 9, 0, 3, 0 },
 					{ 0, 0, 3, 5, 0, 0, 0, 0, 0 },
-					{ 0, 2, 8, 0, 7, 0, 6, 0, 1 },
+					{ 9, 2, 8, 0, 7, 0, 6, 0, 1 },
 					{ 0, 0, 0, 0, 0, 0, 3, 0, 2 },
 					{ 0, 9, 0, 8, 0, 0, 4, 0, 0 },
 					{ 0, 0, 4, 0, 5, 0, 0, 0, 0 },
@@ -58,17 +58,17 @@ public class XuSodoku extends Applet implements ActionListener {
 					{ 3, 7, 4, 9, 8, 2, 5, 6, 1 }
 			},
 			{
-					{,,,,,,,,},
-					{,,,,,,,,},
-					{,,,,,,,,},
-					{,,,,,,,,},
-					{,,,,,,,,},
-					{,,,,,,,,},
-					{,,,,,,,,},
-					{,,,,,,,,},
-					{,,,,,,,,},
+					{ 5, 4, 9, 7, 2, 8, 1, 6, 3 },
+					{ 8, 3, 1, 9, 4, 6, 5, 2, 7 },
+					{ 6, 7, 2, 1, 3, 5, 8, 9, 4 },
+					{ 4, 1, 5, 2, 6, 9, 7, 3, 8 },
+					{ 7, 6, 3, 5, 8, 1, 2, 4, 9 },
+					{ 9, 2, 8, 4, 7, 3, 6, 5, 1 },
+					{ 1, 5, 7, 6, 9, 4, 3, 8, 2 },
+					{ 3, 9, 6, 8, 1, 2, 4, 7, 5 },
+					{ 2, 8, 4, 3, 5, 7, 9, 1, 6 },
 			}
-			
+
 	};
 
 	JLabel level_label, lives_label;
@@ -279,7 +279,7 @@ public class XuSodoku extends Applet implements ActionListener {
 
 	}
 
-	public int[][] get_level(){
+	public int[][] get_level() {
 		int[][] ans = new int[9][9];
 
 		for (int i = 0; i < row; i++) {
