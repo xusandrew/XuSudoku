a74689e6181ec1eb3a3a12ff925b0665bccfe2e5
xusandrew <65879913+xusandrew@users.noreply.github.com>
2022-05-12 01:46:15 -0400

    Add files via upload

---
+++ pics/extra.java
@@ -0,0 +1,13 @@
+// public class extra {
+
+//     public boolean win() {
+//         for (int i = 0; i < row; i++) {
+//             for (int j = 1; j < col - 1; j++) {
+//                 if (sea [i][j-1] == sea [i][j] && sea[i][j+1] == sea [i][j])
+//                     return false;
+//             }
+//         }
+//         return true;
+//     }
+
+// }
